c1b671cb6cfdeae86f57c7ca112454287b5429b0
李华民 <huamin.li@ymm56.com>
2019-01-07 13:55:20 +0800

    contain1

--- shell/contain.sh
+++ shell/contain.sh
@@ -8,3 +8,23 @@ then
 else  
   echo "不包含"
 fi 
+
+## 字符串运算符
+strA="helloworld"
+strB="low"
+if [[ $strA =~ $strB ]] 
+then  
+  echo "包含"
+else  
+  echo "不包含"
+fi 
+
+## 通配符
+A="helloworld"
+B="low"
+if [[ $A = *$B* ]] 
+then  
+  echo "包含"
+else  
+  echo "不包含"
+fi 
